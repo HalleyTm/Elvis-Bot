a29cb7af47da45ca7688afe7c6538743476e7c18
HalleyTm <45395132+HalleyTm@users.noreply.github.com>
2019-01-10 11:04:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,15 @@ const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
 
+    client.on('message', async msg => {
+    if (msg.content.toLowerCase() === prefix + "disko") {
+   if (msg.channel.type === "dm") return;
+  const rol = 'Disko' // Rol ismi buraya
+  setInterval(() => {
+      msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
+      }, 350);
+  }
+
 client.on('message', async message => {
   const ms = require('ms');
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
