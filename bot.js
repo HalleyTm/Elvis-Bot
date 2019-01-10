3b4d4583c220d4a391d6039db35e6118d832a02e
HalleyTm <45395132+HalleyTm@users.noreply.github.com>
2019-01-10 11:12:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,50 +12,20 @@ const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
 
-    client.on('message', async msg => {
-    if (msg.content.toLowerCase() === prefix + "disko") {
-   if (msg.channel.type === "dm") return;
-  const rol = 'Disko' // Rol ismi buraya
-  setInterval(() => {
-      msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
-      }, 350);
-  }
-
-client.on('message', async message => {
-  const ms = require('ms');
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  let u = message.mentions.users.first() || message.author;
-  if (command === "stattemizle") {
-  if (!message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")) return message.channel.send(" İstatistik ayarlanmamış.")
-  if (!message.member.hasPermission('ADMINISTRATOR'))
-  return message.channel.send(" Yetkin bulunmuyor.");
-      const a = message.guild.channels.find(channel => channel.name === "Sunucu İstatistik").delete()
-      if(!a) return console.log("guildStats")
-      const b = message.guild.channels.find(channel => channel.name === `Üye sayısı: ${message.guild.memberCount}`).delete()
-      if(!b) return console.log("guildStatsMember")
-      const c = message.guild.channels.find(channel => channel.name === `Bot sayısı: ${message.guild.members.filter(m => m.user.bot).size}`).delete()
-      if(!c) return console.log("guildStatsBot")
-      const d = message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size}`).delete() //|| message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-2}`).delete()
-      if(!d) return console.log("guildStatsChannel")
-      message.channel.send(" Kanallar temizlendi.")
-    }
-  if (command === "statayarla") {
-  if (message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")) return message.channel.send(" Zaten istatistik ayarlanmış.")
-  if (!message.member.hasPermission('ADMINISTRATOR'))
-  return message.channel.send(" Yetkin bulunmuyor.");
-  message.channel.send(`Kategori ve kanal kurulumu başlatılsın mı? başlatılacak ise **evet** yazınız.`)
-      message.channel.awaitMessages(response => response.content === 'evet', {
-        max: 1,
-        time: 10000,
-        errors: ['time'],
-      })
-    .then((collected) => {
-   message.guild.createChannel('Sunucu İstatistik', 'category', [{
-  id: message.guild.id,
-  deny: ['CONNECT'],
-  deny: ['VIEW_CHANNEL']
-}]);
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
 
 client.on('message', async message => {
   const ms = require('ms');
@@ -76,7 +46,7 @@ client.on('message', async message => {
       if(!d) return console.log("guildStatsChannel")
          const e = message.guild.channels.find(channel => channel.name === `Ping: ${client.ping}`).delete() //|| message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-2}`).delete()
       if(!e) return console.log("guildStatsChannel")
-            const f = message.guild.channels.find(channel => channel.name === `Yapımcım: Ahmet.Beysss`).delete() //|| message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-2}`).delete()
+            const f = message.guild.channels.find(channel => channel.name === `Yapımcım: Emirhan Saraç`).delete() //|| message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-2}`).delete()
       if(!f) return console.log("guildStatsChannel")
                const g = message.guild.channels.find(channel => channel.name === `Kütüphanesi: Discord.js`).delete() //|| message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-2}`).delete()
       if(!g) return console.log("guildStatsChannel")
@@ -114,7 +84,7 @@ message.guild.createChannel(`Toplam Kanal: ${client.channels.size.toLocaleString
 message.guild.createChannel(`Ping: ${client.ping}`, 'voice')
 .then(channel =>
                    channel.setParent(message.guild.channels.find(channel => channel.name === "Bot Kullanımı")));
-message.guild.createChannel(`Yapımcım: Ahmet.Beysss`, 'voice')
+message.guild.createChannel(`Yapımcım: Emirhan Saraç`, 'voice')
 .then(channel =>
                    channel.setParent(message.guild.channels.find(channel => channel.name === "Bot Kullanımı")));
 message.guild.createChannel(`Kütüphanesi: Discord.js`, 'voice')
@@ -125,33 +95,6 @@ message.guild.createChannel(`Kütüphanesi: Discord.js`, 'voice')
         })    
     
 }
-
- message.guild.createChannel(`Üye sayısı: ${message.guild.memberCount}`, 'voice')
-.then(channel =>
- channel.setParent(message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")));
- message.guild.createChannel(`Bot sayısı: ${message.guild.members.filter(m => m.user.bot).size}`, 'voice')
-.then(channel =>
- channel.setParent(message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")));
-message.guild.createChannel(`Kanal sayısı: ${message.guild.channels.size}`, 'voice')
-.then(channel =>
- channel.setParent(message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")));
-  message.channel.send(" Sunucu paneli ayarlandı!")
-        })
-}
-
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
-  if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
-    let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.name}.`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.name);
-    });
-  });
 });
 
 client.reload = command => {
@@ -206,116 +149,6 @@ client.unload = command => {
 };
 
 client.on('message', msg => {
-  const reason = msg.content.split(" ").slice(1).join(" ");
-  if (msg.channel.name== 'destek') { 
-    const hatay = new Discord.RichEmbed()
-    .addField(" Hata ", `Bu Sunucuda \`Destek\` Adında Bir Rol Yok!`)
-    .setColor("RANDOM")
-    
-    if (!msg.guild.roles.exists("name", "Destek")) return msg.author.send(hatay) + msg.guild.owner.send(`${msg.guild.name} Adlı Sunucunda, \`Destek\` Adlı Bir Rol Olmadığı İçin, Hiçkimse Destek Talebi Açamıyor!`);
-    if(msg.guild.channels.find('name', 'Talepler')) {
-      msg.guild.createChannel(`destek-${msg.author.id}`, "text").then(c => {
-      const category = msg.guild.channels.find('name', 'Talepler')
-      c.setParent(category.id)
-      let role = msg.guild.roles.find("name", "Destek");
-      let role2 = msg.guild.roles.find("name", "@everyone");
-      c.overwritePermissions(role, {
-          SEND_MESSAGES: false,
-          READ_MESSAGES: false
-      });
-      c.overwritePermissions(role2, {
-          SEND_MESSAGES: false,
-          READ_MESSAGES: false
-      });
-      c.overwritePermissions(msg.author, {
-          SEND_MESSAGES: true,
-          READ_MESSAGES: true
-      });
-
-      const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setAuthor(`${client.user.username} | Destek Sistemi`)
-      .addField(`Merhaba ${msg.author.username}!`, `Destek Yetkilileri burada seninle ilgilenecektir. \nDestek talebini kapatmak için \`${prefix}kapat\` yazabilirsin.`)
-      .addField(`» Talep Konusu/Sebebi:`, `${msg.content}`, true)
-      .addField(`» Kullanıcı:`, `<@${msg.author.id}>`, true)
-      .setFooter(`${client.user.username} | Destek Sistemi`)
-      .setTimestamp()
-      c.send({ embed: embed });
-      c.send(`<@${msg.author.id}> Adlı kullanıcı "\`${msg.content}\`" sebebi ile destek talebi açtı! Lütfen Destek Ekibini bekle, @here`)
-      msg.delete()
-      }).catch(console.error);
-    }
-  }
-});
-  
-client.on("message", message => {
-if (message.content.toLowerCase().startsWith(prefix + `kapat`)) {
-    if (!message.channel.name.startsWith(`destek-`)) return message.channel.send(`Bu komut sadece Destek Talebi kanallarında kullanılablir!`);
-
-    var deneme = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setAuthor(`Destek Talebi Kapatma İşlemi`)
-    .setDescription(`Destek talebini kapatmayı onaylamak için, \n10 saniye içinde \`evet\` yazınız.`)
-    .setFooter(`${client.user.username} | Destek Sistemi`)
-    message.channel.send(deneme)
-    .then((m) => {
-      message.channel.awaitMessages(response => response.content === 'evet', {
-        max: 1,
-        time: 10000,
-        errors: ['time'],
-      })
-      .then((collected) => {
-          message.channel.delete();
-        })
-        .catch(() => {
-          m.edit('Destek Talebi kapatma isteğin zaman aşımına uğradı!').then(m2 => {
-              m2.delete();
-          }, 3000);
-        });
-    });
-}
-});
-
-const Discord = require('discord.js');
-exports.run = (client, message, args) => {
-  if (message.author.id != "297457504328220673") return message.reply('Bunu Sadece Sahibim Kullanabilir');
-      
-  if (!message.guild) {
-  const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor(0xFF0000)
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
-  .addField('⚠ Uyarı ⚠', 'Bu  komutu özel mesajlarda kullanamazsın.');
-  return message.author.sendEmbed(ozelmesajuyari); }
-  let guild = message.guild;
-  let reason = args.slice(1).join(' ');
-  let user = message.mentions.users.first();
-  if (reason.length < 1) return message.reply('Ne göndericem onuda yazı ver.');
-  if (message.mentions.users.size < 1) return message.reply('Kime Mesaj atacam onuda yazı ver.').catch(console.error);
-  message.delete();
-  message.reply('✔ | Mesajını Gönderdim.')
-  const embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .setTitle(`**Mesaj Sitemi**`)
-  .setTimestamp()
-  .setDescription(reason);
-  return user.send(embed);
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['pm','öm'],
-  permlevel: 4
-};
-
-exports.help = {
-  komut: 'özelmesaj',
-  aciklama: 'Bir kullanıcıya özel mesaj yollar.',
-  kullanim: 'özelmesaj'
-};
-
-client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
     msg.reply('Aleyküm selam,  hoş geldin ^^');
   }
@@ -344,4 +177,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(process.env.ELVIS_BOT);
+client.login(process.env.GNARGE_BOT);
