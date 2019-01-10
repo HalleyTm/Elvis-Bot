e2699fc60cce9a8dddd02cfb6f9b325e7318ad27
HalleyTm <45395132+HalleyTm@users.noreply.github.com>
2019-01-10 11:14:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -177,4 +177,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(process.env.GNARGE_BOT);
+client.login(process.env.GNARGE_BOT1);
